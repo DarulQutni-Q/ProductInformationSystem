bb257b8725b754891f26707b5e1ab9cee1eab892
Darul Qutni <darulqutni512@gmail.com>
2026-09-19 18:36:54 +0700

    buat struktur data produk awal di products.php

---
+++ products.php
@@ -0,0 +1,36 @@
+<?php
+
+$products = [
+    [
+        'id' => 'PRD-001',
+        'nama' => 'Beras Pandan Wangi 5kg',
+        'kategori' => 'Sembako',
+        'harga' => 78500,
+        'stok' => 14,
+        'deskripsi' => 'Beras aroma pandan alami, butiran utuh dan pulen saat dimasak.'
+    ],
+    [
+        'id' => 'PRD-002',
+        'nama' => 'Minyak Goreng Sawit 2L',
+        'kategori' => 'Sembako',
+        'harga' => 34000,
+        'stok' => 10,
+        'deskripsi' => 'Minyak kelapa sawit murni dua kali penyaringan, jernih dan higienis.'
+    ],
+    [
+        'id' => 'PRD-003',
+        'nama' => 'Gula Pasir Kristal Putih 1kg',
+        'kategori' => 'Sembako',
+        'harga' => 17500,
+        'stok' => 25,
+        'deskripsi' => 'Gula kristal tebu alami berkualitas tinggi untuk kebutuhan harian.'
+    ],
+    [
+        'id' => 'PRD-004',
+        'nama' => 'Kopi Robusta Lampung 250g',
+        'kategori' => 'Minuman',
+        'harga' => 32000,
+        'stok' => 8,
+        'deskripsi' => 'Biji kopi robusta pilihan dari dataran tinggi Lampung, sangrai medium.'
+    ]
+];
