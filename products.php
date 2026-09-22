ff51aabbdee95e365b4bba3384ed5b9167d96430
Darul Qutni <darulqutni512@gmail.com>
2026-09-22 10:22:45 +0700

    buat data produk di products.php

---
+++ products.php
@@ -0,0 +1,68 @@
+<?php
+
+$products = [
+    [
+        'id' => 'PRD-001',
+        'nama' => 'Beras Pandan Wangi 5kg',
+        'kategori' => 'Sembako',
+        'harga' => 78500,
+        'stok' => 14,
+        'deskripsi' => 'Beras pulen kualitas premium kemasan 5kg.'
+    ],
+    [
+        'id' => 'PRD-002',
+        'nama' => 'Minyak Goreng Sawit 2L',
+        'kategori' => 'Sembako',
+        'harga' => 34000,
+        'stok' => 2,
+        'deskripsi' => 'Minyak kelapa sawit kemasan pouch.'
+    ],
+    [
+        'id' => 'PRD-003',
+        'nama' => 'Gula Pasir Kristal 1kg',
+        'kategori' => 'Sembako',
+        'harga' => 17500,
+        'stok' => 25,
+        'deskripsi' => 'Gula pasir tebu kristal putih.'
+    ],
+    [
+        'id' => 'PRD-004',
+        'nama' => 'Kopi Robusta Lampung 250g',
+        'kategori' => 'Minuman',
+        'harga' => 32000,
+        'stok' => 1,
+        'deskripsi' => 'Kopi bubuk robusta sangrai medium.'
+    ],
+    [
+        'id' => 'PRD-005',
+        'nama' => 'Teh Celup Melati 50s',
+        'kategori' => 'Minuman',
+        'harga' => 14500,
+        'stok' => 18,
+        'deskripsi' => 'Teh celup melati aroma wangi.'
+    ],
+    [
+        'id' => 'PRD-006',
+        'nama' => 'Susu UHT Full Cream 1L',
+        'kategori' => 'Minuman',
+        'harga' => 21000,
+        'stok' => 0,
+        'deskripsi' => 'Susu sapi cair segar siap minum.'
+    ],
+    [
+        'id' => 'PRD-007',
+        'nama' => 'Garam Beryodium Halus 500g',
+        'kategori' => 'Bumbu Dapur',
+        'harga' => 6500,
+        'stok' => 30,
+        'deskripsi' => 'Garam dapur beryodium butiran halus.'
+    ],
+    [
+        'id' => 'PRD-008',
+        'nama' => 'Kecap Manis Botol 600ml',
+        'kategori' => 'Bumbu Dapur',
+        'harga' => 24000,
+        'stok' => 2,
+        'deskripsi' => 'Kecap manis kedelai hitam tradisional.'
+    ]
+];
